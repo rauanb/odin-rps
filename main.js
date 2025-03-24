021e9e7ced64db38e16c8f4641e0d53abd349956
Rauanb <rauan_b@hotmail.com>
2025-03-24 06:44:47 -0300

    Funcao getComputerChoice

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Hello, JS");
+// Documentação da Mozilla sobre o Math.Random apresentou uma função para pegar inteiros
+// As linhas comentadas são as originais, as seguintes são as alteradas para se adequarem às necessidades do exercício
+
+// function getRandomInt(max) {
+function getRandomInt() {
+    // const inteiro = Math.floor(Math.random() * max);
+    const inteiro = Math.floor(Math.random() * 3);
+    // console.log(inteiro);
+    return inteiro
+}
+
+
+function getComputerChoice() {
+    const opcoes = ['rock', 'paper', 'scissors'];
+    let computer = opcoes[getRandomInt()];
+    console.log(computer)
+}
+// getRandomInt();
+getComputerChoice();
